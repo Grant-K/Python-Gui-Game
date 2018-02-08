d46f31a9ce253e1c9ef8ed1430ad97fd3468c927
GrantK <grantklees108@gmail.com>
2018-02-08 13:47:53 -0800

    Fixed bug, and added addition layers appearing

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import Tkinter
 import math
 import random
 from time import sleep
+from apscheduler.schedulers.background import BackgroundScheduler
+sched = BackgroundScheduler()
 root = Tkinter.Tk()
 canvas = Tkinter.Canvas(root, width=600, height=600, background='#FFFFFF')
 canvas.grid(row=0, rowspan=1, column=1)
@@ -21,29 +23,34 @@ def start(event):
     genBlocks()
     main()
 def genBlocks():
-    x3 = 0
-    x4 = 30
-    y3 = 0
-    y4 = 10
-    global rectangle_items
-    for num in range(0,4):
-        rectangle_items.append(canvas.create_rectangle(x3,y3,x4,y4, outline='#000000', fill='#00FFFF'))
-        x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
-        while(x4 < 600):
-            xDist = random.randint(30,100)
-            rect=canvas.create_rectangle(x4+3,y3,x4+xDist,y4, outline='#000000', fill='#00FFFF')
-            rectangle_items.append(rect)
-            x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
-            
-        x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
-        y3 = y4+10
-        y4 = y3+10
         x3 = 0
-        x4 = random.randint(30,100)
-
+        x4 = 30
+        y3 = 0
+        y4 = 10
+        global rectangle_items
+        if(len(rectangle_items) >= 0):
+            for index in range (0, len(rectangle_items)-1):
+                canvas.move(rectangle_items[index], 0, 10) 
+                sleep(.001)
+        for num in range(0,4):
+            rectangle_items.append(canvas.create_rectangle(x3,y3,x4,y4, outline='#000000', fill='#00FFFF'))
+            x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
+            while(x4 < 600):
+                xDist = random.randint(30,100)
+                rect=canvas.create_rectangle(x4+3,y3,x4+xDist,y4, outline='#000000', fill='#00FFFF')
+                rectangle_items.append(rect)
+                x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
+                
+            x3, y3, x4, y4 = canvas.coords(rectangle_items[len(rectangle_items)-1])
+            y3 = y4+10
+            y4 = y3+10
+            x3 = 0
+            x4 = random.randint(30,100)
+            canvas.update_idletasks()
 def main():
     # Get the slider data and create x- and y-components of velocity
     global direction
+    global rectangle_items
     velocity_x = speed * math.cos(direction) # adj = hyp*cos()
     velocity_y = speed * math.sin(direction) # opp = hyp*sin()
     # Change the canvas item's coordinates
@@ -62,7 +69,7 @@ def main():
     # causing recursion      
     rx1, ry1, rx2, ry2 = canvas.coords(rectangle_item)
     if circle_item in canvas.find_overlapping(rx1,ry1,rx2,ry2):
-        direction = -1 * direction
+        direction = direction * -1
         for n in range(1, 5):
             velocity_x = speed * math.cos(direction)
             velocity_y = speed * math.sin(direction)
@@ -82,18 +89,19 @@ def main():
     if(len(rectangle_items) < 0):
             genBlocks()
     else:
-        for index in range(0, len(rectangle_items)-1):
-            x3, y3, x4, y4 = canvas.coords(rectangle_items[index])
-            if(circle_item in canvas.find_overlapping(x3, y3, x4, y4)):
-                canvas.delete(rectangle_items[index])
-                print(len(rectangle_items))
-                print(index)
-                for i in range(0, len(rectangle_items)-1):
-                    test1,test2, test3, test4 = canvas.coords(rectangle_items[i])
-                    print(test1, test2, test3, test4)
+        for index in range(0, ((len(rectangle_items))-1)):
+            if(index < len(rectangle_items)):
+                x3, y3, x4, y4 = canvas.coords(rectangle_items[index])
+                if(circle_item in canvas.find_overlapping(x3, y3, x4, y4)):
+                    canvas.delete(rectangle_items[index])
+                    del rectangle_items[index]
+                    direction = direction * -1
                     
-    canvas.after(1, main)
+    canvas.after(5, main)
 # Call function directly to start the recursion
 canvas.bind("<Button-1>", start)
-
+sched.start()
+sched.add_job(genBlocks, 'interval', seconds=10)
 root.mainloop()
+
+sched.shutdown()
